8efdca129c34478bc1aaaf1548a2c959d87f7128
thornyy <razaabbaskhokhar123@gmail.com>
2026-09-23 18:46:09 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+def temp(fahrenheit):
+    return 9/5*(fahrenheit - 32)
+
+app = gr.Interface(
+    fn=temp,
+    inputs=gr.Number(label="Temperature In Fahrenheit"),
+    outputs=gr.Number(label="Temperature In Celsius"),
+)
+
+app.launch(
+  server_name="0.0.0.0",
+  server_port=int(os.environ.get("PORT",7860))
+)
